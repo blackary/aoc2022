24e6f2d9c4324bd3a0fc72ac3c2baa516246a39d
Zachary Blackwood <zblackwo@gmail.com>
2022-12-08 22:02:21 -0500

    Finish day05

--- day05.py
+++ day05.py
@@ -1,14 +1,13 @@
+from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Tuple
 
-from dataclasses import dataclass
-
 full_input = Path("day05").read_text()
 
-test_input = """    [D]    
-[N] [C]    
+test_input = """    [D]
+[N] [C]
 [Z] [M] [P]
- 1   2   3 
+ 1   2   3
 
 move 1 from 2 to 1
 move 3 from 1 to 3
@@ -25,15 +24,13 @@ class Move():
 Stack = List[str]
 
 def split(raw_input: str) -> Tuple[List[Stack], List[Move]]:
-  # Split into stacks and move instructions
-
   stacks_string, move_instructions = raw_input.split("\n\n")
 
-  num_stacks = int(len(stacks_string.splitlines()[0]) / 3)
+  num_stacks = int((len(stacks_string.splitlines()[-2]) + 1) / 4)
 
-  stacks: list[Stack] = [[]*num_stacks]
+  stacks: list[Stack] = [list() for _ in range(num_stacks)]
   for row in stacks_string.splitlines()[:-1]:
-    letters = row[1::3]
+    letters = row[1::4]
     print(letters)
     for stack_idx, letter in enumerate(letters):
       if letter != " ":
@@ -47,13 +44,21 @@ def split(raw_input: str) -> Tuple[List[Stack], List[Move]]:
     to_stack = int(move_instruction[5])
     moves.append(Move(num, from_stack, to_stack))
 
+  print(stacks)
+  print(moves)
   return stacks, moves
 
 
 def move_one(stacks: List[Stack], move: Move):
   for i in range(move.num):
-    to_move = stacks[move.from_stack].pop()
-    stacks[move.to_stack].append(to_move)
+    to_move = stacks[move.from_stack - 1].pop()
+    stacks[move.to_stack - 1].append(to_move)
+
+
+def move_one_groups(stacks: List[Stack], move: Move):
+  to_move = stacks[move.from_stack - 1][-move.num:]
+  stacks[move.from_stack - 1] = stacks[move.from_stack - 1][:-move.num]
+  stacks[move.to_stack - 1].extend(to_move)
 
 def move_all(raw_input: str) -> str:
   stacks, moves = split(raw_input)
@@ -63,4 +68,19 @@ def move_all(raw_input: str) -> str:
 
   return "".join(stack[-1] for stack in stacks)
 
+
+def move_all_groups(raw_input: str) -> str:
+  stacks, moves = split(raw_input)
+
+  for move in moves:
+    move_one_groups(stacks, move)
+
+  return "".join(stack[-1] for stack in stacks)
+
 assert move_all(test_input) == "CMZ"
+
+print(move_all(full_input))
+
+assert move_all_groups(test_input) == "MCD"
+
+print(move_all_groups(full_input))
