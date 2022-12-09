02fef5b905a1742b6bf04d10c4ae15760dcb0e9a
Zachary Blackwood <zachary@streamlit.io>
2022-12-09 02:05:09 +0000

    Initial commit

--- day02_B.py
+++ day02_B.py
@@ -1,16 +1,23 @@
 """
 The winner of the whole tournament is the player with the highest score. Your total score is the sum of your scores for each round. The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+
+"Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
 """
 from pathlib import Path
 
 ROCK = "Rock"
 PAPER = "Paper"
 SCISSORS = "Scissors"
+LOSE = "Lose"
+DRAW = "Draw"
+WIN = "Win"
 
-ME = {"A": ROCK, "B": PAPER, "C": SCISSORS}
-THEM = {"X": ROCK, "Y": PAPER, "Z": SCISSORS}
+THEM = {"A": ROCK, "B": PAPER, "C": SCISSORS}
+ME = {"X": ROCK, "Y": PAPER, "Z": SCISSORS}
+ME_LOOKUP = {v: k for k, v in ME.items()}
+ME_RESULT = {"X": LOSE, "Y": DRAW, "Z": WIN}
 
-inputs = Path("input2").read_text().splitlines()
+inputs = Path("input02").read_text().splitlines()
 
 test_inputs = """A Y
 B X
@@ -56,28 +63,52 @@ def get_score(me_letter: str, them_letter: str) -> int:
   return score
 
 
+def get_my_letter(them_letter, me_result) -> str:
+  if ME_RESULT[me_result] == DRAW:
+    return ME_LOOKUP[THEM[them_letter]]
+
+  if ME_RESULT[me_result] == WIN:
+    if THEM[them_letter] == ROCK:
+      return ME_LOOKUP[PAPER]
+    elif THEM[them_letter] == PAPER:
+      return ME_LOOKUP[SCISSORS]
+    else:
+      return ME_LOOKUP[ROCK]
+
+  if ME_RESULT[me_result] == LOSE:
+    if THEM[them_letter] == ROCK:
+      return ME_LOOKUP[SCISSORS]
+    elif THEM[them_letter] == PAPER:
+      return ME_LOOKUP[ROCK]
+    else:
+      return ME_LOOKUP[PAPER]
+
+
 def get_total_score(input_lines) -> int:
   my_score = 0
   for line in input_lines:
-    me_letter, them_letter = line.split(" ")
+    them_letter, me_result = line.split(" ")
+    me_letter = get_my_letter(them_letter, me_result)
+    #print(them_letter, me_result, me_letter)
     my_score += get_score(me_letter, them_letter)
     # print(line, my_score)
   return my_score
 
 
-assert get_total_score(test_inputs) == 15
-
+assert get_total_score(test_inputs) == 12
+_ = """
 assert get_total_score(["A X"]) == 1 + 3
-assert get_total_score(["A Y"]) == 1 + 0
-assert get_total_score(["A Z"]) == 1 + 6
+assert get_total_score(["A Y"]) == 2 + 6
+assert get_total_score(["A Z"]) == 3 + 0
 
-assert get_total_score(["B X"]) == 2 + 6
+assert get_total_score(["B X"]) == 1 + 0
 assert get_total_score(["B Y"]) == 2 + 3
-assert get_total_score(["B Z"]) == 2 + 0
+assert get_total_score(["B Z"]) == 3 + 6
 
-assert get_total_score(["C X"]) == 3 + 0
-assert get_total_score(["C Y"]) == 3 + 6
+assert get_total_score(["C X"]) == 1 + 6
+assert get_total_score(["C Y"]) == 2 + 0
 assert get_total_score(["C Z"]) == 3 + 3
+"""
 
 print(len(inputs))
 print(get_total_score(inputs))
---
+++ day03.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from string import ascii_letters
+from more_itertools import chunked
+
+test_lines = Path("day03_test").read_text().splitlines()
+lines = Path("day03").read_text().splitlines()
+
+def get_priority(line) -> int:
+  half = int(len(line)/2)
+  first, second = line[:half], line[half:]
+  common_letter = list(set(first).intersection(second))[0]
+  score = ascii_letters.index(common_letter) + 1
+  return score
+
+def get_priorities(lines):
+  return sum(get_priority(l) for l in lines)
+
+assert get_priorities(test_lines) == 157
+
+print(get_priorities(lines))
+
+def get_common(lines):
+  return list(set(lines[0]).intersection(lines[1]).intersection(lines[2]))[0]
+
+def score_groups(lines):
+  score = 0
+  for chunk in chunked(lines, 3):
+    common = get_common(chunk) 
+    score += ascii_letters.index(common) + 1
+  return score
+
+print(score_groups(lines))
+
+ 
